12d97d76ebfaad3044a26a64544b973795de0976
mattiwennberg <matti.wennberg@piql.com>
2014-05-29 21:51:20 +0200

    Get input files from script arguments instead of hardcoding them

--- tools/ehealth1-sip-creator.php
+++ tools/ehealth1-sip-creator.php
@@ -1,26 +1,157 @@
 <?php
 include(__DIR__ . '/../includes/ehealth1-sip.php');
 
+// Help text
+if ($argc != 3)
+{
+    echo "Usage: " . $argv[0] . " <IP directory> <schema directory>" . PHP_EOL;
+    exit(1);
+}
+
+// IP direcotry
+$informationPackageDirectory = $argv[1];
+if (!is_dir($informationPackageDirectory))
+{
+    echo "IP directory is invalid: $informationPackageDirectory" . PHP_EOL;
+}
+
+// Schema direcotry
+$schemaDirectory = $argv[2];
+if (!is_dir($schemaDirectory))
+{
+    echo "Schema directory is invalid: $schemaDirectory" . PHP_EOL;
+}
+
 $outputDir = './';
-$informationPackageId = 'SAMPLE_ID';
+
+// Get ID of informationpackage
+$parts = explode("_", basename($informationPackageDirectory));
+$informationPackageId = $parts[count($parts)-1];
 
 // Define SIP
 $outPath;
 $sip = new Ehealth1Sip($informationPackageId);
-$sip->addSubmissionAgreement('./SA.pdf');
 
-// Add patients
-// TODO: Create form script arguments
-$patient1 = new Ehealth1SipPatient('PATIENT1_ID');
-$patient1->addFile('commit-transaction.php', 'commit-transaction.php');
-$patient1->addFile('commit-transaction-ack.php', 'commit-transaction-ack.php');
-$sip->addPatient($patient1);
+// Schema
+$schemaFiles = scandir($schemaDirectory);
+if ($schemaFiles === false)
+{
+    echo "Failed to read schema directory: {$schemaDirectory}";
+    exit(1);
+}
+foreach ($schemaFiles as $schemaFile)
+{
+    if (substr($schemaFile, 0, 1) == '.')
+    {
+        // Skipping file
+        continue;
+    }
+    $sip->addSchemaFile("{$schemaDirectory}/{$schemaFile}");
+}
 
-$patient2 = new Ehealth1SipPatient('PATIENT2_ID');
-$patient2->addFile('includes/filetransfer', 'filetransfer');
-$patient2->addFile('includes/filetransfer/file-transfer-base.php', 'filetransfer/file-transfer-base.php');
-$patient2->addFile('includes/filetransfer/file-transfer-ssh.php', 'filetransfer/file-transfer-ssh.php');
-$sip->addPatient($patient2);
+// Submission agreement
+$submissionAgreementPath = "{$informationPackageDirectory}/Submissionagreement_{$informationPackageId}.pdf";
+if (!file_exists($submissionAgreementPath))
+{
+    echo "Submission agreement does not exist: {$submissionAgreementPath}" . PHP_EOL;
+    exit(1);
+}
+$sip->addSubmissionAgreement($submissionAgreementPath);
+
+// Add files in root level of informationpackage
+$files = scandir($informationPackageDirectory);
+if ($files === false)
+{
+    echo "Failed to read IP directory: {$informationPackageDirectory}";
+    exit(1);
+}
+foreach ($files as $file)
+{
+    $filePath = "{$informationPackageDirectory}/{$file}";
+    if (substr($file, 0, 1) == '.')
+    {
+        // Skipping file
+        continue;
+    }
+
+    if ($sip->isSubmissionAgreement($filePath))
+    {
+        // Add submission agreement
+        $sip->addSubmissionAgreement($filePath);
+    }
+    else if ($sip->isDescriptiveMetadata($filePath))
+    {
+        // Add descriptive metadata
+        $sip->addDescriptiveMetadata($filePath);
+    }
+    else if (is_dir($filePath) && basename($filePath) == 'Data')
+    {
+        $patientDirectories = scandir($filePath);
+        if ($patientDirectories === false)
+        {
+            echo "Failed to read patient directories from data directory: {$filePath}";
+            exit(1);
+        }
+
+        foreach ($patientDirectories as $patientDirectory)
+        {
+            $patientPath = "{$filePath}/{$patientDirectory}";
+
+            if (substr($patientDirectory, 0, 1) == '.')
+            {
+                // Skipping file
+                continue;
+            }
+
+            if (!is_dir($patientPath))
+            {
+                echo "Invalid file: {$patientPath}" . PHP_EOL;
+                exit(1);
+            }
+
+            // Define patient
+            $parts = explode('_', basename($patientPath));
+            if (count($parts) != 2)
+            {
+                echo "Patient directory has invalid naming: {$patientPath}" . PHP_EOL;
+                exit(1);
+            }
+            $patient = new Ehealth1SipPatient($parts[1]);
+
+            $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($patientPath, RecursiveDirectoryIterator::SKIP_DOTS));
+            foreach ($rii as $patientFile)
+            {
+                $patientFilePath = $patientFile->getPathname();
+                $relativePatientFilePath = substr($patientFilePath, strlen($patientPath)+1);
+
+                if ($patient->isDescriptiveMetadata($relativePatientFilePath))
+                {
+                    // Add descriptive metadata
+                    $patient->addDescriptiveMetadata($patientFilePath);
+                }
+                else
+                {
+                    // Add payload
+                    $patient->addFile($patientFilePath, $relativePatientFilePath);
+                }
+            }
+
+            // Check that patient has data files
+            if (count($patient->files()) == 0)
+            {
+                echo "Patient has no data files" . PHP_EOL;
+            }
+
+            // Add patient to SIP
+            $sip->addPatient($patient);
+        }
+    }
+    else
+    {
+        echo "File was not recognized as SIP data: {$filePath}";
+        exit(1);
+    }
+}
 
 // Output SIP
 $outPath;
